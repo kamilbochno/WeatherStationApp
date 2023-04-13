c04d177c3b1bf36df1b81c28d9b5bbbf4ba6cb35
Kamil <bochno.kamil@gmail.com>
2023-04-13 23:07:40 +0200

    Added dashboard screen

---
+++ screens/dashboard.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Dashboard = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
