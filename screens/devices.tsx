cd43adac5dfa221d1132282225a492b5883c80c8
Kamil <bochno.kamil@gmail.com>
2023-04-13 23:06:28 +0200

    Added devices screen

---
+++ screens/devices.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Devices = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Devices</Text>
+    </View>
+  );
+};
+
+export default Devices;
